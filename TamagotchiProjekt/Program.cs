65583a7d16fa422438c19c35ab72c23e408df702
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2023-10-06 09:59:00 +0200

    initial

---
+++ TamagotchiProjekt/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Tamagotchi intern = new();
+
+Console.WriteLine("What is your unpaid interns name?");
+intern.name = Console.ReadLine();
+
+while (intern.GetAlive())
+{
+    Console.WriteLine("\nWhat do you wish to do now? \nTeach your unpaid intern a new word [type: teach] \nGreet your unpaid intern [type: greet] \nFeed your unpaid intern [type: feed] \nDo nothing [don't type the above]");
+    string action = Console.ReadLine().ToLower(); 
+    Console.WriteLine();
+
+    if (action == "teach")
+    {
+        intern.Teach();
+        intern.Tick();
+    }
+    else if (action == "greet")
+    {
+        intern.Hi();
+        intern.Tick();
+    }
+    else if (action == "feed")
+    {
+        intern.Feed();
+        intern.Tick();
+    }
+    else
+    {
+        intern.Tick();
+        Console.WriteLine("You did nothing.");
+    }
+    intern.PrintStats();
+}
+
+Console.WriteLine("End");
+Console.ReadLine();
---
+++ TamagotchiProjekt/Tamagotchi.cs
@@ -0,0 +1,81 @@
+﻿
+public class Tamagotchi
+{
+    int hunger;
+    int boredom;
+    List<string> words = new();
+    bool isAlive = true;
+    Random generator = new();
+    public string name;
+
+    public void Feed()
+    {
+        hunger -= 2;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+        Console.WriteLine($"You fed {name}.");
+    }
+
+    public void Hi()
+    {
+        if (words.Count > 0)
+        {
+            int index = generator.Next(words.Count);
+            Console.WriteLine($"{words[index]}");
+            ReduceBoredom();
+        }
+        else 
+        {
+            Console.WriteLine($"{name} does not know any words to greet you with so {name} just gives you the finger."); 
+        }
+    }
+
+    public void Teach()
+    {
+        Console.WriteLine($"What word do you wish to teach {name}?");
+        string word = Console.ReadLine();
+        words.Add(word);
+        ReduceBoredom();
+        Console.WriteLine($"You taught your unpaid intern the word: {word}.");
+    }
+
+    public void Tick()
+    {
+        hunger++;
+        boredom++;
+
+        if (hunger > 10 || boredom > 10)
+        {
+            isAlive = false;
+        }
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"\nHunger: {hunger}\nBoredom: {boredom}");
+        if (isAlive == true)
+        {
+            Console.WriteLine($"{name} is alive.");
+        }
+        else
+        {
+            Console.WriteLine($"{name} is dead, lol.");
+        }
+    }
+
+    public bool GetAlive()
+    {
+        return isAlive;
+    }
+
+    void ReduceBoredom()
+    {
+        boredom -= 2;
+        if (boredom < 0)
+        {
+            boredom = 0;
+        }
+    }
+}
